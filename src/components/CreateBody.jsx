8f5140af210df35477c1b67c251d751291dbea67
ejPark43 <pej22200310@handong.ac.kr>
2024-07-11 20:09:51 +0900

    feat: 가격, 저장버튼 생성

--- src/components/CreateBody.jsx
+++ src/components/CreateBody.jsx
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
 import styled from "styled-components";
 function CreateBody() {
   const [nameCounter, setNameCounter] = useState(0);
@@ -9,83 +10,107 @@ function CreateBody() {
   const writeDescription = (e) => {
     setDescribeField(e.target.value.length);
   };
+
+  const submitPost = async (event) => {
+    navigate("/");
+  };
+
+  const navigate = useNavigate();
   return (
     <div>
-      <MainBody>
-        <div className="body-center">
-          <UploadImgBox className="box">
-            <PartTitle>
-              상품이미지 <span>(0/12)</span>
-            </PartTitle>
-            <div className="img-box">
-              <div className="img-upload">이미지 등록</div>
-              상품 이미지는 pc에서는 1:1, 모바일에서는 1:1.23 비율로 보여져요.
+      <form onSubmit={submitPost}>
+        <MainBody>
+          <div className="body-center">
+            <UploadImgBox className="box">
+              <PartTitle>
+                상품이미지 <span>(0/12)</span>
+              </PartTitle>
+              <div className="img-box">
+                <div className="img-upload">이미지 등록</div>
+                상품 이미지는 pc에서는 1:1, 모바일에서는 1:1.23 비율로 보여져요.
+              </div>
+            </UploadImgBox>
+            <div className="box name-box">
+              <PartTitle>상품명</PartTitle>
+              <div className="name-box-input">
+                <input
+                  placeholder="상품명을 입력해주세요."
+                  onChange={writeName}
+                  maxLength={40} // 상품명 최대 길이 40글자
+                ></input>
+                <span>{nameCounter}/40</span>
+              </div>
             </div>
-          </UploadImgBox>
-          <div className="box name-box">
-            <PartTitle>상품명</PartTitle>
-            <div className="name-box-input">
-              <input
-                placeholder="상품명을 입력해주세요."
-                onChange={writeName}
-                maxLength={40} // 상품명 최대 길이 40글자
-              ></input>
-              <span>{nameCounter}/40</span>
+            <div className="box category-box">
+              <PartTitle>카테고리</PartTitle>
+              <div className="category">
+                <div className="category-select"></div>
+                <span>선택한 카테고리: </span>
+              </div>
             </div>
-          </div>
-          <div className="box category-box">
-            <PartTitle>카테고리</PartTitle>
-            <div className="category">
-              <div className="category-select"></div>
-              <span>선택한 카테고리: </span>
+            <div className="box status-box">
+              <PartTitle>상품상태</PartTitle>
+              <RadioGroup>
+                <label>
+                  <input type="radio" name="status" value="new" />새
+                  상품(미사용){" "}
+                  <span className="status-detail">사용하지 않은 새 상품</span>
+                </label>
+                <label>
+                  <input type="radio" name="status" value="like-new" />
+                  사용감 없음{" "}
+                  <span>사용은 했지만 눈에 띄는 흔적이나 얼룩이 없음</span>
+                </label>
+                <label>
+                  <input type="radio" name="status" value="used" />
+                  사용감 적음 <span>눈에 띄는 흔적이나 얼룩이 약간 있음</span>
+                </label>
+                <label>
+                  <input type="radio" name="status" value="worn" />
+                  사용감 많음 <span>눈에 띄는 흔적이나 얼룩이 많이 있음</span>
+                </label>
+                <label>
+                  <input type="radio" name="status" value="needs-repair" />
+                  고장/파손 상품{" "}
+                  <span>기능 이상이나 외관 손상 등으로 수리/수선 필요</span>
+                </label>
+              </RadioGroup>
             </div>
-          </div>
-          <div className="box status-box">
-            <PartTitle>상품상태</PartTitle>
-            <RadioGroup>
-              <label>
-                <input type="radio" name="status" value="new" />새 상품(미사용){" "}
-                <span className="status-detail">사용하지 않은 새 상품</span>
-              </label>
-              <label>
-                <input type="radio" name="status" value="like-new" />
-                사용감 없음{" "}
-                <span>사용은 했지만 눈에 띄는 흔적이나 얼룩이 없음</span>
-              </label>
-              <label>
-                <input type="radio" name="status" value="used" />
-                사용감 적음 <span>눈에 띄는 흔적이나 얼룩이 약간 있음</span>
-              </label>
-              <label>
-                <input type="radio" name="status" value="worn" />
-                사용감 많음 <span>눈에 띄는 흔적이나 얼룩이 많이 있음</span>
-              </label>
-              <label>
-                <input type="radio" name="status" value="needs-repair" />
-                고장/파손 상품{" "}
-                <span>기능 이상이나 외관 손상 등으로 수리/수선 필요</span>
-              </label>
-            </RadioGroup>
-          </div>
-          <div className="box describe-box">
-            <PartTitle>설명</PartTitle>
-            <div className="describe-box-input">
-              <textarea
-                placeholder={`브랜드, 모델명, 구매 시기, 하자 유무 등 상품 설명을 최대한 자세히 적어주세요.
+            <div className="box describe-box">
+              <PartTitle>설명</PartTitle>
+              <div className="describe-box-input">
+                <textarea
+                  placeholder={`브랜드, 모델명, 구매 시기, 하자 유무 등 상품 설명을 최대한 자세히 적어주세요.
 전화번호, sns 계정 등 개인정보 입력은 제한될 수 있어요.
 안전하고 건전한 거래 환경을 위해 과학기술정보통신부, 한국인터넷증흥원과 번개장터(주)가 함께합니다.
               `}
-                onChange={writeDescription}
-                maxLength={2000}
-              ></textarea>
-              <span>{describeField}/2000</span>
+                  onChange={writeDescription}
+                  maxLength={2000}
+                ></textarea>
+                <span>{describeField}/2000</span>
+              </div>
             </div>
-          </div>
-          <br />
+            <br />
+            <div className="box price-box">
+              <PartTitle>가격</PartTitle>
+              <div>
+                {/* <input placeholder="가격을 입력해주세요. "> */}
+                <input
+                  placeholder="가격을 입력해주세요. "
+                  type="number"
+                  min="10"
+                  max="99999999"
+                />
 
-          <PartTitle>가격</PartTitle>
-        </div>
-      </MainBody>
+                {/* </input> */}
+              </div>
+            </div>
+            <button type="submit" className="submitBtnInModal">
+              저장
+            </button>
+          </div>
+        </MainBody>
+      </form>
     </div>
   );
 }
@@ -188,6 +213,36 @@ const MainBody = styled.div`
           justify-content: flex-end;
           width: 102%; /// <<<< 나중에 수정
         }
+      }
+    }
+
+    .price-box {
+      display: flex;
+      > div {
+        > input {
+          outline: none;
+          border: 1px solid gray;
+          /* height: 40px; */
+          padding: 10px;
+          width: 200px;
+        }
+      }
+    }
+
+    > button {
+      align-self: flex-end;
+      margin-top: 30px;
+      height: 40px;
+      outline: none;
+      border: none;
+      color: white;
+      cursor: pointer;
+      width: 300px;
+
+      /* border: 2px solid black; */
+      background-color: #f5443b;
+      &:hover {
+        background-color: #c82921;
       }
     }
   }
