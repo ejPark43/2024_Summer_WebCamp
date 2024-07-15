6407d247d618d220b74edd72f1e38e308c532ecc
ejPark43 <pej22200310@handong.ac.kr>
2024-07-15 18:09:50 +0900

    feat: footer 임시 생성

--- src/pages/CreatePage.jsx
+++ src/pages/CreatePage.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import Header from "../components/Header";
 import CreateBody from "../components/CreateBody";
+import Footer from "../components/Footer";
 
 function CreatePage() {
   return (
     <>
       <Header />
       <CreateBody />
+      <Footer />
     </>
   );
 }
--- src/pages/MainPage.jsx
+++ src/pages/MainPage.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import Header from "../components/Header";
 import BodyContent from "../components/BodyContent";
+import Footer from "../components/Footer";
 
 function MainPage() {
   return (
     <div>
       <Header />
       <BodyContent />
+      <Footer />
     </div>
   );
 }
